50dbc401ef49a950173159295df82789bef28baa
Riza Fahmi <rizafahmi@gmail.com>
2019-02-11 08:51:44 +0700

    Placeholder for firebase data

--- src/App.js
+++ src/App.js
@@ -8,17 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Data from firebase will be here...</p>
         </header>
       </div>
     );

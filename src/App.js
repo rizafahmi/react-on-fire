fcff6da5a166c85022a6e885c6b412a5ff786958
Riza Fahmi <rizafahmi@gmail.com>
2019-02-11 09:24:11 +0700

    Connect to firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import { database } from './firebase.js';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null
+    };
+  }
+  componentDidMount() {
+    database.ref('/').on('value', () => {
+      console.log('DATA CHANGED!');
+    });
+  }
   render() {
     return (
       <div className="App">
